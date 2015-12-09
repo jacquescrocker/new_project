2dec73af119df56a2d7400a459cdb934fd889f70
Jacques Crocker <railsjedi@gmail.com>
2015-12-09 00:26:46 -0800

    try adding react on rails

--- Gemfile
+++ Gemfile
@@ -25,9 +25,12 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'bootstrap-sass'
+
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem "react_on_rails", "1.2.0.rc1"
 
 gem 'active_model_serializers'
 gem 'annotate'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,11 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+// DO NOT REQUIRE jQuery or jQuery-ujs in this file!
+// DO NOT REQUIRE TREE!
+
+//= require react_on_rails
+
+// NOTE: vendor-bundle exposes jquery and jquery-ujs
+//= require generated/vendor-bundle
+//= require generated/app-bundle
+
+// NOTE: bootstrap-sprockets depends on generated/vendor-bundle for jQuery.
+//= require bootstrap-sprockets
